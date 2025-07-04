c2d90a3628b7c09eae016ffb202a6f302ce3cd90
Thanush <thanushdinesh04@gmail.com>
2025-07-04 16:24:23 +0530

    Phase 3 chnages

--- client/public/js/config.js
+++ client/public/js/config.js
@@ -1,11 +1,11 @@
 const config = {
     development: {
-        API_BASE_URL: 'http://54.146.215.18:3001/api',
+        API_BASE_URL: 'http://localhost:3001/api',
         DEBUG: true,
         LOG_LEVEL: 'debug'
     },
     production: {
-        API_BASE_URL: '/api',
+        API_BASE_URL: 'http://54.146.215.18:3001/api', // <-- your backend API URL
         DEBUG: false,
         LOG_LEVEL: 'error'
     }
